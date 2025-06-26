4d22620c8f82abdce829281401b6972e404e4b13
Harrison Wang <harrisonized@gmail.com>
2025-06-26 19:23:29 -0400

    build function to calculate positions for each significance bar for an arbitrary number of bars

--- R/functions/plotting.R
+++ R/functions/plotting.R
@@ -2,7 +2,7 @@ import::here(zeallot, '%<-%')
 import::here(magrittr, '%>%')
 import::here(superb, 'showSignificance')
 import::here(ggplot2,
-    'ggplot', 'aes', 'aes_string', 'geom_boxplot', 'geom_jitter',
+    'ggplot', 'aes', 'geom_boxplot', 'geom_jitter',
     'stat_summary', 'scale_fill_brewer', 'scale_y_continuous', 'expansion',
     'ggtitle')
 import::here(file.path(wd, 'R', 'config', 'lasers.R'),
@@ -10,6 +10,7 @@ import::here(file.path(wd, 'R', 'config', 'lasers.R'),
 
 ## Functions
 ## compute_nlevels
+## generate_start_positions
 ## plot_spectra_by_each_laser
 ## plot_violin_with_significance
 
@@ -27,6 +28,39 @@ compute_nlevels <- function(n) {
         nlevels <- m*(m+1)
     }
     return(nlevels)
+}
+
+
+#' Generate Starting Positions
+#' 
+#' Calculate the correction factor for the bar height of each p_value bar
+#' Used for plot_violin_with_significance
+#' 
+generate_start_positions <- function(n_groups) {
+
+    if ( n_groups == 1 ) {
+        return(0)
+    } else if ( n_groups == 2 ) {
+        return(1)
+    }
+
+    # compute number of levels within the group
+    mid <- floor(n_groups/2)
+    if ( (n_groups%%2) == 0 ) {
+        nlevels <- c(1:mid, (mid-1):1)  # even
+    } else {
+        nlevels <- c(1:mid, (mid):1)  # odd
+    }
+
+    res <- c()
+    value <- 1
+    nreps <- n_groups-1
+    for (step in nlevels) {
+        res <- c(res, rep(value, nreps))
+        value <- value + step
+        nreps <- nreps - 1
+    }
+    return(res)
 }
 
 
@@ -107,12 +141,15 @@ plot_violin_with_significance <- function(
 ) {
 
     # compute bar positions
+    # this should be cached for quick lookup
     n_groups <- length(unique(df[[x]]))
     nlevels <- compute_nlevels(n_groups)
 
     id_combos <- setNames(as.data.frame(t(combn(1:n_groups, 2))), c('a', 'b'))
     id_combos[['diff']] <- id_combos[['b']]-id_combos[['a']]
     id_combos <- id_combos[order(id_combos[['diff']]), ]
+    id_combos[['start_pos']] <- generate_start_positions(n_groups)
+    id_combos[['level']] <- (id_combos[['a']]-1) %% id_combos[['diff']] + id_combos[['start_pos']]
 
     # compute starting height
     h_low <- max(aggregate(df[[y]],
@@ -120,23 +157,35 @@ plot_violin_with_significance <- function(
     )[['x']])*1.1
     space <- h_low/10
 
-    fig <- ggplot(df, aes_string(x=x, y=y, fill=x)) + 
-        geom_boxplot(alpha=0.7, aes_string(middle=y)) +
+    fig <- ggplot(df, aes(x=.data[[x]], y=.data[[y]], fill=.data[[x]])) + 
+        geom_boxplot(alpha=0.7, aes(middle=.data[[y]])) +
         stat_summary(fun=mean, geom="crossbar", width=0.75, linewidth=0.25, linetype = "dashed") +
         geom_jitter() +
         scale_fill_brewer(palette="Dark2") +
         scale_y_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.1))) +
-        ggtitle(title) +
+        ggtitle(title)
 
-        # need a loop for arbitrary numbers
+
+
+        # need a loop for arbitrary number of groups
+
+        # level 0
         showSignificance( c(1.1,1.9), h_low, -0.005, round(pval_tbl[['pval_1v2']], 4)) +
         showSignificance( c(2.1,2.9), h_low, -0.005, round(pval_tbl[['pval_2v3']], 4)) +
         showSignificance( c(3.1,3.9), h_low, -0.005, round(pval_tbl[['pval_3v4']], 4)) +
+        # showSignificance( c(4.1,4.9), h_low, -0.005, round(pval_tbl[['pval_4v5']], 4)) +
 
+        # level 1 (alternating space starting with h_low+space)
         showSignificance( c(1.1,2.9), h_low + space, -0.005, round(pval_tbl[['pval_1v3']], 4)) +
         showSignificance( c(2.1,3.9), h_low + 2*space, -0.005, round(pval_tbl[['pval_2v4']], 4)) +
+        # showSignificance( c(3.1,4.9), h_low + space, -0.005, round(pval_tbl[['pval_3v5']], 4)) +
 
+        # level 2
         showSignificance( c(1.1,3.9), h_low + 3*space, -0.005, round(pval_tbl[['pval_1v4']], 4))
+        # showSignificance( c(2.1,4.9), h_low + 4*space, -0.005, round(pval_tbl[['pval_2v5']], 4))
+
+        # level 3
+        # showSignificance( c(1.1,4.9), h_low + 5*space, -0.005, round(pval_tbl[['pval_1v5']], 4))
 
     return(fig)
 }
